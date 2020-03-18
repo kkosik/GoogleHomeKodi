96de7615561c247598ca45be9def8fb00b449fa7
kkosik <kylekosik@hotmail.com>
2020-03-18 10:29:22 -0400

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -986,7 +986,7 @@ const fadeVolume = (Kodi, newVolume, oldVolume) => { // eslint-disable-line no-u
             
             console.log('Vol: ${currentVolume}');
         }, 50);
-    };
+    });
     
     changeVol();
     
